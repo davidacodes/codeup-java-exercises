9c1a07c40539544cf6612a1c825a62018be42a89
davidalex620 <spand3r@gmail.com>
2018-06-09 14:13:32 -0500

    working on Java practice problems

---
+++ src/practice/ClubBouncer.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class ClubBouncer {
+    public static void main(String[] args) {
+        int age = 22;
+        boolean onGuestList = false;
+        double allure = 7.5;
+        String gender = "F";
+
+
+    }
+}
