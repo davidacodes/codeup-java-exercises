04c06899f43784d94ed6f7d04e9dd7622d433f6c
davidalex620 <spand3r@gmail.com>
2018-06-10 10:30:00 -0500

    working on Java practice problems

--- src/practice/ClubBouncer.java
+++ src/practice/ClubBouncer.java
@@ -7,6 +7,11 @@ public class ClubBouncer {
         double allure = 7.5;
         String gender = "F";
 
-
+        if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8)) {
+            System.out.println("You are allowed to enter the club.");
+        }
+        else {
+            System.out.println("You are not allowed to enter the club.");
+        }
     }
 }
